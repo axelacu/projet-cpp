ac87a81cd1dc33814e3cdbe2d4dc01311d51d0d0
axelacu <axel.acuna@dauphine.eu>
2020-05-14 07:35:24 +0200

    Changing files structure

---
+++ test/DESTest/basicTest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by axel_ on 13/05/2020.
+//
+
+#include <include/gtest/gtest.h>
+#include <test.h>
+
+TEST(basic_test,toto_test){
+    ASSERT_TRUE(toto());
+}
+
+TEST(basic_test,eq_test){
+    ASSERT_TRUE(equal(5,5));
+    ASSERT_FALSE(equal(2,4));
+}
